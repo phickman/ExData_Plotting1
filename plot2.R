ec28736a44aaedd1080e52309d7018fda89d76b2
Paul Hickman <paul_hickman@hotmail.com>
2015-10-08 20:52:45 +0800

    New plot2

--- plot2.R
+++ plot2.R
@@ -1,14 +1,13 @@
 
 data <- read.table("../household_power_consumption.txt", sep=";", header=TRUE, na.strings="?")
 
-##data$Date_Time = strptime(paste(data$Date, data$Time),"%d/%m/%Y %H:%M:%S")
+data$Date_Time = strptime(paste(data$Date, data$Time),"%d/%m/%Y %H:%M:%S")
 data$Date = as.Date(data$Date,"%d/%m/%Y")
 
 data <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
 
-png("plot1.png", height=480, width=480)
+png("plot2.png", height=480, width=480)
 
-hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+plot(data$Date_Time, data$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
 
-#dev.copy(png,file="plot1.png")
 dev.off()
